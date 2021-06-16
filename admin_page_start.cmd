2bc904e01c8b5e9b8c997a6f9087b59bb1abc68c
David <zkffhtm6523@naver.com>
2021-06-16 23:34:29 +0900

    pybo 추가

---
+++ admin_page_start.cmd
@@ -0,0 +1,4 @@
+@echo off
+d:
+cd "d:\2_Develop\2021 Project\admin_page"
+"d:\2_Develop\2021 Project\venvs\admin_page\Scripts\activate"
---
+++ admin_page_stop.cmd
@@ -0,0 +1,3 @@
+@echo off
+d:
+"d:\2_Develop\2021 Project\venvs\admin_page\Scripts\deactivate"
