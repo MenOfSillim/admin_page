b79756d7e05b24facf527c26dfea68258aa9a654
David <zkffhtm6523@naver.com>
2021-06-16 23:42:47 +0900

    pybo 추가 url 매핑

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from hello import views
+
+from pybo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('pybo/', views.index),
 ]
